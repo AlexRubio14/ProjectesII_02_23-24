fae91de5bb5ef5a88e09cdc2601023ba0a3d29fa
Crami <crami.andrada@gmail.com>
2023-11-04 18:07:45 +0100

    Updates started

---
+++ Assets/Project/Scripts/Player/NeededUpgradePlayer.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NeededUpgradePlayer : MonoBehaviour
+{
+    private SpriteRenderer c_itemToShowSR;
+
+    [SerializeField]
+    private Sprite interactKeySprite;
+
+    public void ShowNeededUpgrade(UpgradeObject _upgrade)
+    {
+        if (_upgrade)
+        {
+
+        }
+
+
+    }
+
+    public void HideNeededUpgrade()
+    {
+
+    }
+}
