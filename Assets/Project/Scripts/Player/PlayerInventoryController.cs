a6c7cb8488fc05f86f48533909be008f48a7f258
Judit Espigol <judith.espigol@enti.cat>
2023-12-14 23:21:11 +0100

    fixed conflicts

--- Assets/Project/Scripts/Player/PlayerInventoryController.cs
+++ Assets/Project/Scripts/Player/PlayerInventoryController.cs
@@ -23,6 +23,5 @@ public class PlayerInventoryController : MonoBehaviour
     private void InventoryAction(InputAction.CallbackContext obj)
     {
         inventory.SetActive(!inventory.activeInHierarchy);
-        Debug.Log("sexo");
     }
 }
