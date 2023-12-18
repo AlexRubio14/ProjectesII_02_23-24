bbd1e71a026c40aa3966fae9e3d9a83464c223c3
Crami <crami.andrada@gmail.com>
2023-12-18 21:32:29 +0100

    Auto aim done

--- Assets/Project/Scripts/Player/Cannon/CannonController.cs
+++ Assets/Project/Scripts/Player/Cannon/CannonController.cs
@@ -6,8 +6,6 @@ using UnityEngine.InputSystem;
 public class CannonController : MonoBehaviour
 {
     [Header("Input"), SerializeField]
-    private InputActionReference aimTurretAction;
-    [SerializeField]
     private InputActionReference shootAction;
 
     [Space, Header("External References"), SerializeField]
@@ -16,77 +14,103 @@ public class CannonController : MonoBehaviour
     private GameObject laserPrefab;
 
     [Space, Header("Shoot"), SerializeField]
+    private float shootingRange;
+    [SerializeField]
     private float reloadDelay;
     private float currentDelay;
     private bool isShooting;
     [SerializeField]
     private float fuelConsume;
-    private Vector2 turretAimDirection;
     [SerializeField]
-    private PlayerController.State[] canShootStates; 
+    private PlayerController.State[] canShootStates;
+    private Rigidbody2D nearestEnemy;
+    [SerializeField]
+    private LayerMask enemiesMask;
 
-    private InputController iController;
+    [Space, SerializeField]
+    private GameObject aimTarget;
+
+    [Space, SerializeField]
+    private bool autoShoot;
+
     private PlayerController playerController;
 
     private void Awake()
     {
-        iController = GetComponentInParent<InputController>();
         playerController = GetComponentInParent<PlayerController>();
         isShooting = false;
     }
 
     private void OnEnable()
     {
-        aimTurretAction.action.started += AimTurretAction;
-        aimTurretAction.action.performed += AimTurretAction;
-        aimTurretAction.action.canceled += AimTurretAction;
-
         shootAction.action.started += ShootAction;
         shootAction.action.canceled += ShootAction;
     }
 
     private void OnDisable()
     {
-        aimTurretAction.action.started -= AimTurretAction;
-        aimTurretAction.action.performed -= AimTurretAction;
-        aimTurretAction.action.canceled -= AimTurretAction;
-
         shootAction.action.started -= ShootAction;
         shootAction.action.canceled -= ShootAction;
     }
 
     private void FixedUpdate()
     {
+        CheckNearestEnemy();
         RotateCanon();
         Shoot();
     }
 
+    private void CheckNearestEnemy()
+    {
+        RaycastHit2D[] hits = Physics2D.CircleCastAll(transform.position, shootingRange, Vector2.zero, 0, enemiesMask);
+
+        float minDisntance = 100;
+        Rigidbody2D foundEnemy = null;
+        foreach (RaycastHit2D hit in hits)
+        {
+            if (minDisntance > hit.distance)
+            {
+                foundEnemy = hit.rigidbody;
+                minDisntance = hit.distance;
+            }
+        }
+
+        nearestEnemy = foundEnemy;
+
+        if (nearestEnemy)
+        {
+            if (!aimTarget.activeInHierarchy)
+            {
+                aimTarget.SetActive(true);
+            }
+
+            aimTarget.transform.position = nearestEnemy.transform.position;
+        }
+        else if (aimTarget.activeInHierarchy)
+            aimTarget.SetActive(false);
+    }
     private void RotateCanon()
     {
-        if(iController.GetCurrentControllerType() == InputController.ControllerType.KEYBOARD) 
-        {
-            Vector2 direction = (GetMousePosition() - (Vector2)transform.position).normalized;
+        if (!nearestEnemy)
+            return;
 
-            transform.up = direction;
-        }
-        else
-        {
-            if (turretAimDirection.normalized == Vector2.zero)
-            {
-                return;
-            }
-            transform.up = turretAimDirection.normalized;
-        }
+        Vector2 direction = (nearestEnemy.transform.position - transform.position).normalized;
+        transform.up = direction;
     }
     public void Shoot()
     {
         currentDelay += Time.fixedDeltaTime;
+        if (autoShoot)
+        {
+            isShooting = nearestEnemy;
+        }
+        
 
         if (currentDelay >= reloadDelay && isShooting && CheckPlayerState())
         {
             currentDelay = 0;
             Instantiate(laserPrefab, posToSpawnBullets.position, transform.rotation);
-            CameraController.Instance.SetTrauma(0.4f);
+            CameraController.Instance.SetTrauma(0.3f);
             playerController.SubstractHealth(fuelConsume);
         }
     }
@@ -109,21 +133,15 @@ public class CannonController : MonoBehaviour
 
 
     #region Input
-    private void AimTurretAction(InputAction.CallbackContext obj)
-    {
-        turretAimDirection = aimTurretAction.action.ReadValue<Vector2>();
-    }
     private void ShootAction(InputAction.CallbackContext obj)
     {
         isShooting = obj.action.IsInProgress();
     }
-
-    Vector2 GetMousePosition()
-    {
-        Vector2 mousePosition = Input.mousePosition;
-        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
-        return mousePosition;
-    }
-
     #endregion
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawWireSphere(transform.position, shootingRange);
+    }
 }
