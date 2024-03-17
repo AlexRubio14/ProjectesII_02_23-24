2b70ac3298b3bde868c47896aacb6b15f60ed820
Judit Espigol <judith.espigol@enti.cat>
2024-03-17 13:26:40 +0100

    wind effect material almost done

---
+++ Assets/Project/Scripts/Enviroment/GrassExternalVelocityTrigger.cs
@@ -0,0 +1,111 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+public class GrassExternalVelocityTrigger : MonoBehaviour
+{
+    private GrassVelocityController grassVelocityController;
+
+    private GameObject player;
+
+    private Material material; 
+
+    private Rigidbody2D playerRb2D;
+
+    private bool easeInCoroutineRunning;
+    private bool easeOutCoroutineRunning;
+
+    private int externalInfluence = Shader.PropertyToID("_ExternalInfluence");
+
+    private float startingXVelocity;
+    private float velocityLastFrame;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player"); 
+        playerRb2D = player.GetComponent<Rigidbody2D>();
+        grassVelocityController = GetComponentInParent<GrassVelocityController>();
+
+        material = GetComponent<SpriteRenderer>().material;
+        startingXVelocity = material.GetFloat(externalInfluence); 
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject == player)
+        {
+            if (!easeInCoroutineRunning && Mathf.Abs(playerRb2D.velocity.x) < Mathf.Abs(grassVelocityController.VelocityThreshold))
+            {
+                StartCoroutine(EaseIn(playerRb2D.velocity.x * grassVelocityController.ExternalInfluenceStrength));
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject == player)
+        {
+            StartCoroutine(EaseOut());
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject == player)
+        {
+            if (Mathf.Abs(velocityLastFrame) < MathF.Abs(grassVelocityController.VelocityThreshold) && 
+                MathF.Abs(playerRb2D.velocity.x) > Mathf.Abs(grassVelocityController.VelocityThreshold))
+            {
+                StartCoroutine(EaseOut()); 
+            }
+            else if (Mathf.Abs(velocityLastFrame) > Mathf.Abs(grassVelocityController.VelocityThreshold) && 
+                Mathf.Abs(playerRb2D.velocity.x) < Mathf.Abs(grassVelocityController.VelocityThreshold))
+            {
+                StartCoroutine(EaseIn(playerRb2D.velocity.x * grassVelocityController.ExternalInfluenceStrength)); 
+            }
+            else if (!easeInCoroutineRunning && !easeOutCoroutineRunning &&
+                MathF.Abs(playerRb2D.velocity.x) > Mathf.Abs(grassVelocityController.VelocityThreshold))
+            {
+                grassVelocityController.InfluenceGrass(material, playerRb2D.velocity.x * grassVelocityController.ExternalInfluenceStrength);
+            }
+
+            velocityLastFrame = playerRb2D.velocity.x;
+        }
+    }
+
+    private IEnumerator EaseIn(float XVelocity)
+    {
+        easeInCoroutineRunning = true;
+
+        float elapsedTime = 0f;
+        while(elapsedTime < grassVelocityController.EaseInTime)
+        {
+            elapsedTime += Time.deltaTime;
+
+            float lerpedAmount = Mathf.Lerp(startingXVelocity, XVelocity, (elapsedTime / grassVelocityController.EaseInTime)); 
+            grassVelocityController.InfluenceGrass(material, lerpedAmount);
+
+            yield return null;
+        }
+
+        easeInCoroutineRunning = false;
+    }
+    private IEnumerator EaseOut()
+    {
+        easeOutCoroutineRunning = true;
+        float currentXInfluence = material.GetFloat(externalInfluence);
+
+        float elapsedTime = 0f; 
+        while(elapsedTime < grassVelocityController.EaseOutTime)
+        {
+            elapsedTime += Time.deltaTime;
+
+            float lerpedAmount = Mathf.Lerp(currentXInfluence, startingXVelocity, (elapsedTime/ grassVelocityController.EaseOutTime));
+            grassVelocityController.InfluenceGrass(material, lerpedAmount); 
+
+            yield return null;  
+        }
+
+        easeOutCoroutineRunning = false;
+    }
+}
